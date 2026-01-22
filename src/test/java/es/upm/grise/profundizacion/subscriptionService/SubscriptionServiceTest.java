6a2339928f611ac7a154d292df5327fff7ec18da
Frank Marco Alvarado Grefa <frankmarcogrefa@gmail.com>
2026-01-22 08:40:39 -0500

    Actualizado

--- src/test/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionServiceTest.java
+++ src/test/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionServiceTest.java
@@ -1,10 +1,64 @@
 package es.upm.grise.profundizacion.subscriptionService;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import es.upm.grise.profundizacion.exceptions.ExistingUserException;
+import es.upm.grise.profundizacion.exceptions.LocalUserDoesNotHaveNullEmailException;
+import es.upm.grise.profundizacion.exceptions.NullUserException;
 
 public class SubscriptionServiceTest {
-	
-	@Test
-	public void smokeTest() {}
-	
+
+    @Test
+    public void smokeTest() {
+        new SubscriptionService();
+    }
+
+    @Test
+    public void testAddSubscriberCorrect() throws Exception {
+        SubscriptionService service = new SubscriptionService();
+        User user = new User(Delivery.DO_NOT_DELIVER, "test@email.com");
+
+        service.addSubscriber(user);
+
+        assertEquals(1, service.getSubscribers().size());
+        assertTrue(service.getSubscribers().contains(user));
+    }
+
+
+    @Test(expected = NullUserException.class)
+    public void testAddSubscriberNullUser() throws Exception {
+        SubscriptionService service = new SubscriptionService();
+        service.addSubscriber(null);
+    }
+
+
+    @Test(expected = ExistingUserException.class)
+    public void testAddSubscriberExistingUser() throws Exception {
+        SubscriptionService service = new SubscriptionService();
+        User user = new User(Delivery.DO_NOT_DELIVER, "test@email.com");
+
+        service.addSubscriber(user);
+        service.addSubscriber(user); // repetido
+    }
+
+    
+    @Test(expected = LocalUserDoesNotHaveNullEmailException.class)
+    public void testAddSubscriberLocalUserWithEmail() throws Exception {
+        SubscriptionService service = new SubscriptionService();
+        User user = new User(Delivery.LOCAL, "local@email.com");
+
+        service.addSubscriber(user);
+    }
+
+  
+    @Test
+    public void testAddSubscriberLocalUserWithNullEmail() throws Exception {
+        SubscriptionService service = new SubscriptionService();
+        User user = new User(Delivery.LOCAL, null);
+
+        service.addSubscriber(user);
+
+        assertEquals(1, service.getSubscribers().size());
+    }
 }
